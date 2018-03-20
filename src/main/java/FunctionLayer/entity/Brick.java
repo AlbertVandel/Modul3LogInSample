0f5a134695b6b7666d14ab24c43839f39a785a2f
Albert Vandel <albertvandel@yahoo.dk>
2018-03-20 20:32:46 +0100

    tiny additions

---
+++ src/main/java/FunctionLayer/entity/Brick.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FunctionLayer.entity;
+
+/**
+ *
+ * @author alber
+ */
+public class Brick {
+    private int height;
+    private int width;
+    private int length;
+
+    public Brick(int height, int width, int length) {
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Brick{" + "height=" + height + ", width=" + width + ", length=" + length + '}';
+    }
+}
---
+++ src/main/webapp/WEB-INF/products.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : products
+    Created on : 20-03-2018, 15:45:34
+    Author     : alber
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Products</title>
+    </head>
+    <body>
+        <h1>Session running as <%=request.getParameter( "email" )%> </h1>
+        <h1>Vælg brikker til dit legohus</h1>
+        
+    </body>
+</html>
